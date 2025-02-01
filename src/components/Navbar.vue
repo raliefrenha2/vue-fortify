fef64d88aa47b9638f8a93a7b96ae27beb1f759d
Romi Alief Rahman <raliefrenha2@gmail.com>
2025-02-01 17:23:56 +0700

    add LoginView

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="navbar bg-base-100">
     <div class="flex-1">
-      <a class="btn btn-ghost text-xl">daisyUI</a>
+      <a class="btn btn-ghost text-3xl">daisyUI</a>
     </div>
     <div class="flex-none">
       <ul class="menu menu-horizontal px-1">
         <li><RouterLink :to="{ name: 'home' }">Home</RouterLink></li>
         <li><RouterLink :to="{ name: 'about' }">About</RouterLink></li>
-        <li></li>
+        <li><RouterLink :to="{ name: 'login' }">Login</RouterLink></li>
       </ul>
     </div>
   </div>
